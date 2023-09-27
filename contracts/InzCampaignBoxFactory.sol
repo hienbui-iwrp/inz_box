83d0b719a098b09cee41292860983b3560fceb61
DucllOwO <nguyenduc147862@gmail.com>
2023-09-27 12:05:53 +0700

    add local variable for contracts

---
+++ contracts/InzCampaignBoxFactory.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import {Clones} from "@openzeppelin/contracts/proxy/Clones.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+
+contract InZCampaignBoxFactory {
+    // address of the contract implement box logic
+    address private boxImplementation;
+    // box campaign have been cloned by factory
+    address[] private boxCampaigns;
+}
